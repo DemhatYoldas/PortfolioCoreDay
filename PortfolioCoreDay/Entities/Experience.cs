c833ab14a92a6f6c8696566bc77070c1fb559e26
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2026-05-12 13:55:38 +0300

    Add project files.

---
+++ PortfolioCoreDay/Entities/Experience.cs
@@ -0,0 +1,12 @@
+﻿namespace PortfolioCoreDay.Entities
+{
+    public class Experience
+    {
+        public int ExperinceId { get; set; }
+        public string Company { get; set; }
+        public string Department { get; set; }
+        public string ExperienceDate { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ PortfolioCoreDay/Entities/Services.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioCoreDay.Entities
+{
+    public class Services
+    {
+        public int ServiceId { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public bool Status { get; set; }
+    }
+}

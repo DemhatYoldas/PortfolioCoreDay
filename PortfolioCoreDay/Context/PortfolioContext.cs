08b3e722b45e724eccb96d7a14486cad2334cfac
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2026-05-13 15:23:23 +0300

    feat: configure database connection and DbContext

---
+++ PortfolioCoreDay/Context/PortfolioContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioCoreDay.Entities;
+
+namespace PortfolioCoreDay.Context
+{
+    public class PortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JQG1QG1;inital Catalog=PortfolioDayDb;integrated security=true;");
+        }
+
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}

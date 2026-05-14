b120821b8488f1c354ef7d7dd248f6eb742bb048
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2026-05-14 17:22:28 +0300

    insert skills entities and testimonial crud

--- PortfolioCoreDay/Controllers/TestimonialController.cs
+++ PortfolioCoreDay/Controllers/TestimonialController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 using PortfolioCoreDay.Context;
 using PortfolioCoreDay.Entities;
 
@@ -6,27 +7,51 @@ namespace PortfolioCoreDay.Controllers
 {
     public class TestimonialController : Controller
     {
-        PortfolioContext context=new PortfolioContext();
+        PortfolioContext context = new PortfolioContext();
         public IActionResult TestimonialListe()
         {
-            var values=context.Testimonials.ToList();
+            var values = context.Testimonials.ToList();
             return View(values);
             // sayfa yenilenmesiyle beraber verilerde gelsin 
         }
 
         [HttpGet]
-          public IActionResult CreateTestimonial()
+        public IActionResult CreateTestimonial()
         {
             return View();
         }
 
 
         [HttpPost]
-        public IActionResult PostTestimonial(Testimonial testimonial) 
+        public IActionResult CreateTestimonial(Testimonial testimonial)
         {
             context.Testimonials.Add(testimonial);
             context.SaveChanges();
             return RedirectToAction("TestimonialListe");
+        }
+
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var value = context.Testimonials.Find(id);
+            context.Testimonials.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("TestimonialListe");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateTestimonial(int id)
+        {
+            var value = context.Testimonials.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            context.Testimonials.Update(testimonial);
+            context.SaveChanges();
+            return RedirectToAction("TestimonialListe");
+
         }
     }
 }
---
+++ PortfolioCoreDay/Entities/Skill.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioCoreDay.Entities
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string SkillName { get; set; }
+        public int SkillValue { get; set; }
+    }
+}

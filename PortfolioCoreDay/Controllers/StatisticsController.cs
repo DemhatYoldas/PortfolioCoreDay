cb91ebdaca47d91677a99c70b2b30ab8dcee21a1
DEMHAT <DEMHAT@DESKTOP-JQG1QG1>
2026-05-15 16:01:43 +0300

    Add statistics view for skills with summary metrics
    Implemented statistics in StatisticsController to display skill count, sum, average, and count above 80 using PortfolioContext. Added Index.cshtml to present these metrics with styled Bootstrap alerts and integrated with the AdminLayout.

--- PortfolioCoreDay/Controllers/StatisticsController.cs
+++ PortfolioCoreDay/Controllers/StatisticsController.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioCoreDay.Context;
 
 namespace PortfolioCoreDay.Controllers
 {
     public class StatisticsController : Controller
     {
+        PortfolioContext context=new PortfolioContext();
         public IActionResult Index()
         {
+            ViewBag.vCount=context.Skills.Count();
+            ViewBag.vSum=context.Skills.Sum(x=>x.SkillValue);
+            ViewBag.vAvg=context.Skills.Average(x=>x.SkillValue);
+            ViewBag.vMax=context.Skills.Where(x=>x.SkillValue>80).Count();
             return View();
         }
     }
